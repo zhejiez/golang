f2e2df42b339278e5c0563300e4541835743f089
zhejie <zhejiez@qq.com>
2019-09-26 10:08:24 +0800

    负数改为复数

--- 文档/5_数据类型/6_复数类型.go
+++ 文档/5_数据类型/6_复数类型.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	var t complex128
 	t = 2.1 + 3.14i
 	fmt.Println("t = ", t)
@@ -12,7 +12,7 @@ func main()  {
 	fmt.Printf("t2 type is %T\n", t2)
 
 	//通过内奸函数,取实部和虚部
-	fmt.Println("real(t2)=", real(t2), ", imag(t2)=" , imag(t2))
+	fmt.Println("real(t2)=", real(t2), ", imag(t2)=", imag(t2))
 }
 
 //返回结果
@@ -20,4 +20,4 @@ func main()  {
 t =  (2.1+3.14i)
 t2 type is complex128
 real(t2)= 3.3 , imag(t2)= 4.4
- */
+*/
